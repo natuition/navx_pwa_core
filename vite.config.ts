8b04b3551a2ba43f4d1685334b5dbac83855534d
Vincent LAMBERT <vincent.lambertvlb@gmail.com>
2025-10-13 17:30:57 +0200

    github page modif

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: (import.meta as any).env?.GH_PAGES_BASE || '/',
+  base: process.env.GH_PAGES_BASE || '/',
   plugins: [
     react(),
     VitePWA({
